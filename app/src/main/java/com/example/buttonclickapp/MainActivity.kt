17df896ccd193873689985373745178625ab5fbb
Octavian Duminica <duminicaoctavian@gmail.com>
2020-10-07 21:47:05 +0300

    The Logcat Pane

--- app/src/main/java/com/example/buttonclickapp/MainActivity.kt
+++ app/src/main/java/com/example/buttonclickapp/MainActivity.kt
@@ -3,19 +3,23 @@ package com.example.buttonclickapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.method.ScrollingMovementMethod
+import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 
+private val TAG = "MainActivity"
+
 class MainActivity : AppCompatActivity() {
 
-//    private var userInput: EditText? = null
-//    private var button: Button? = null
+    //    private var userInput: EditText? = null
+    //    private var button: Button? = null
     private var textView: TextView? = null
     //private var numTimesClicked = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        Log.d(TAG, "onCreate: called")
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
@@ -35,6 +39,7 @@ class MainActivity : AppCompatActivity() {
 //                } else {
 //                    textView?.append("\n")
 //                }
+                Log.d(TAG, "onClick: called")
                 textView?.append(userInput.text)
                 textView?.append("\n")
 //                userInput.text.clear()
